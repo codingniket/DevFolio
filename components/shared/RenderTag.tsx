ded292038425b8491ab51b444750b837036d752f
codingniket <niketbachhawat7@gmail.com>
2023-10-28 00:02:45 +0530

    doen

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -6,7 +6,7 @@ interface Props {
   _id: string;
   name: string;
   totalQuestions?: number;
-  showCount?: number;
+  showCount?: boolean;
 }
 
 const RenderTag = ({ _id, name, totalQuestions, showCount }: Props) => {
